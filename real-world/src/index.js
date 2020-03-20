b599928fc69a1fc2a07f9c325b7dc19b3a2c9168
Muhanned <muhannedkamil2005@gmail.com>
2020-03-20 01:09:55 -0400

    Build simple reduc store , and add the command for install redux

--- real-world/src/index.js
+++ real-world/src/index.js
@@ -1,10 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDOM from "react-dom";
+import React from "react";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {createStore} from 'redux';
+import { Provider } from "react-redux";
+
+import * as serviceWorker from "./serviceWorker";
+
+const defaultState ={checked:false};
+const reducer = function(state = defaultState, action) {
+  switch (action.type) {
+    case 'TOGGLE' :
+      return {...state,checked:!state.checked}
+    default :
+      return {...state}
+  }
+  // return state => This is another way to return the default state in case of initial action
+};
+const store = createStore(reducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, World!</h1>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
